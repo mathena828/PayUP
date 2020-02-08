e0c896fd326a9cd9fe96c1addecb5e2151b20349
mathena828 <mathena828@gmail.com>
2020-02-08 13:31:35 +0800

    Heroku

--- PayUP/settings.py
+++ PayUP/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.runserver_nostatic',
 ]
 
 ROOT_URLCONF = 'PayUP.urls'
